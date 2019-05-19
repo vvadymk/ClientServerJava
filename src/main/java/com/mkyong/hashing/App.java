e8f0bc8b02bcd67b5bb1020e0efd73df18c3dcf9
vvadymk <kochmar.vadym@gmail.com>
2019-05-19 19:25:18 +0300

    Initial commit

---
+++ src/main/java/com/mkyong/hashing/App.java
@@ -0,0 +1,17 @@
+package com.mkyong.hashing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+}
---
+++ src/test/java/com/mkyong/hashing/AppTest.java
@@ -0,0 +1,8 @@
+package com.mkyong.hashing;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+}
